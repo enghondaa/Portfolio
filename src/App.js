297b226e5d57282eb5a03ba6a237fc63b2bfb06f
enghondaa <eng.mohand2389@gmail.com>
2022-11-06 23:21:25 +0200

    First update

--- src/App.js
+++ src/App.js
@@ -3,22 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className='vh-100'>
+        <div className='container h-100 d-flex justify-content-center align-items-center'>
+            <h1>Honda</h1>
+        </div>
+      </section>
+    </>
   );
 }
 
